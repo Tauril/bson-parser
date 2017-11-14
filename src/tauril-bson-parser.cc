23267d32bcade7681ef25587343f3a459528bbc1
Guillaume Marques <marque_o@acu.epita.fr>
2017-11-14 01:49:41 +0100

    bson: set up main file
    * tauril-bson-parser.cc: Here.

---
+++ src/tauril-bson-parser.cc
@@ -0,0 +1,44 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "bson-parser.hh"
+
+static void help()
+{
+  std::cout << "Usage: ./src/tauril-bson-parser file.bson\n";
+}
+
+int main(int argc, char** argv)
+{
+  if (argc != 2)
+  {
+    help();
+    return 1;
+  }
+
+  try
+  {
+    // Load file and store content inside the buffer.
+    std::ifstream file(argv[1]);
+    if (!file.is_open())
+      throw std::invalid_argument;
+
+    std::vector<char> buffer;
+
+    while (file.good())
+      buffer.push_back(file.get());
+
+    // Init the parser.
+    Document doc;
+    Bson::BsonParser parser(buffer, doc);
+
+    doc.Dump();
+  } catch (const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 2;
+  }
+
+  return 0;
+}
