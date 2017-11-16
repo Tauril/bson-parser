8c3ef5d6d3e681aa78952a5b0622574b4eeac69e
Guillaume Marques <marque_o@acu.epita.fr>
2017-11-16 02:17:09 +0100

    ast: element: magnify Type comments
    * src/ast/element.hh: Here.

--- src/ast/element.hh
+++ src/ast/element.hh
@@ -12,34 +12,35 @@ namespace Ast
     // Type of element.
     enum class Type
     {
-      /*  0 */ _double,
-      /*  1 */ _string,
-      /*  2 */ _embedded_doc,
-      /*  3 */ _array,
-      /*  4 */ _binary_data,
-      /*  5 */ _undefined, // Deprecated
-      /*  6 */ _objectid,
-      /*  7 */ _false,
-      /*  8 */ _true,
-      /*  9 */ _datetime,
-      /* 10 */ _null,
-      /* 11 */ _regex,
-      /* 12 */ _db_pointer,
-      /* 13 */ _js_code,
-      /* 14 */ _symbol, // Deprecated
-      /* 15 */ _js_code_ws,
-      /* 16 */ _int32,
-      /* 17 */ _uint64,
-      /* 18 */ _int64,
-      /* 19 */ _decimal128,
-      /* 20 */ _min_key,
-      /* 21 */ _max_key
+      /* \x01 */ _double,
+      /* \x02 */ _string,
+      /* \x03 */ _embedded_doc,
+      /* \x04 */ _array,
+      /* \x05 */ _binary_data,
+      /* \x06 */ _undefined, // Deprecated
+      /* \x07 */ _objectid,
+      /* \x08 */ _false,
+      /* \x08 */ _true,
+      /* \x09 */ _datetime,
+      /* \x0A */ _null,
+      /* \x0B */ _regex,
+      /* \x0C */ _db_pointer,
+      /* \x0D */ _js_code,
+      /* \x0E */ _symbol, // Deprecated
+      /* \x0F */ _js_code_ws,
+      /* \x10 */ _int32,
+      /* \x11 */ _uint64,
+      /* \x12 */ _int64,
+      /* \x13 */ _decimal128,
+      /* \xFF */ _min_key,
+      /* \x7F */ _max_key
     };
 
     Element(Element::Type type, const EName& name, T attr);
 
     Element::Type type_get() const;
     EName name_get() const;
+    // TODO: SFINAE getter for smart pointer and for literal types
     T attr_get() const;
   private:
     // Type of element
