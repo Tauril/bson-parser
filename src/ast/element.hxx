ea3ebd1fede22fbffb1d3355bd8f372624fd28bb
Guillaume Marques <marque_o@acu.epita.fr>
2017-11-17 10:11:41 +0100

    ast: element: move ctor into header file
    The template class constructor was generating some linker issues.
    
    * Makefile.am,
    * src/ast/element.cc,
    * src/ast/element.hxx: Here.

--- src/ast/element.hxx
+++ src/ast/element.hxx
@@ -6,6 +6,13 @@ namespace Ast
 {
 
   template <typename T>
+  Element<T>::Element(ElementAttribute::Type type, const EName& name, T attr)
+    : type_(type)
+    , name_(name)
+    , attr_(attr)
+  {}
+
+  template <typename T>
   inline ElementAttribute::Type
   Element<T>::type_get() const
   {
